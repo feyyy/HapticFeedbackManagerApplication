115c355a2ac786dbb20988ef1bd35c8d22cde485
eskimosehir <rizadoogan@gmail.com>
2020-04-22 17:02:38 +0300

    toggle implemented

---
+++ HapticFeedbackManagerApplication/Assets/Scripts/UI/ToggleButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour {
+
+    [SerializeField] private Transform predefinedVibrationContainer;
+    [SerializeField] private Transform customVibrationContainer;
+    private bool _isPredefinedActive;
+
+    public void OnClick() {
+        _isPredefinedActive = !_isPredefinedActive;
+        if (_isPredefinedActive) {
+            predefinedVibrationContainer.gameObject.SetActive(true);
+            customVibrationContainer.gameObject.SetActive(false);
+        } else {
+            predefinedVibrationContainer.gameObject.SetActive(false);
+            customVibrationContainer.gameObject.SetActive(true);
+        }
+        
+        var scale = transform.localScale;
+        scale.x = -scale.x;
+        transform.localScale = scale;
+    }
+}
